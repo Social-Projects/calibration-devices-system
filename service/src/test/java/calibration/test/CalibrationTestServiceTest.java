35ce5d9942126c3fdc135647ff395c0947b6d350
volodymyrignatiev <volodymyrignatiev94@gmail.com>
2015-08-14 09:55:39 +0300

    first commit

---
+++ service/src/test/java/calibration/test/CalibrationTestServiceTest.java
@@ -0,0 +1,61 @@
+package calibration.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vova on 12.08.15.
+ */
+public class CalibrationTestServiceTest {
+
+    @Test
+    public void testFindTestById() throws Exception {
+
+    }
+
+    @Test
+    public void testFindByVerificationId() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAllCalibrationTests() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCalibrationTestsBySearchAndPagination() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateNewTest() throws Exception {
+
+    }
+
+    @Test
+    public void testEditTest() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteTest() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateTestData() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAllTestDataAsociatedWithTest() throws Exception {
+
+    }
+
+    @Test
+    public void testUploadPhotos() throws Exception {
+
+    }
+}
---
+++ service/src/test/java/calibration/test/DeviceServiceTest.java
@@ -0,0 +1,36 @@
+package calibration.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vova on 12.08.15.
+ */
+public class DeviceServiceTest {
+
+    @Test
+    public void testExistsWithDeviceid() throws Exception {
+
+    }
+
+    @Test
+    public void testGetById() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDevicesBySearchAndPagination() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllByType() throws Exception {
+
+    }
+}
